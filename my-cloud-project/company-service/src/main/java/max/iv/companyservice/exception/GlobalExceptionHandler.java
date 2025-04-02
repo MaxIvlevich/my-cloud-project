ca447a40955455e544eb1ecaeb89084837c62f3b
Maxivich <maxivlevich@gmail.com>
2025-04-02 19:19:01 +0300

    created some Tests

--- my-cloud-project/company-service/src/main/java/max/iv/companyservice/exception/GlobalExceptionHandler.java
+++ my-cloud-project/company-service/src/main/java/max/iv/companyservice/exception/GlobalExceptionHandler.java
@@ -9,12 +9,13 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.stream.Collectors;
 
 @Slf4j
-@AllArgsConstructor
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
